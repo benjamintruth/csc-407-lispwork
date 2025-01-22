224755331c85ff866ac2ce92882ef0edf2b8b758
Benajmin Sanning <bsanning@healthwarehouse.com>
2025-01-22 12:29:17 -0500

    emoty skeleton

---
+++ lisp_test.lisp
@@ -0,0 +1,14 @@
+(defun print-ln (item) (format t "~a~%" item))
+
+
+(defun main () 
+
+    ;; drop some newline chars to space output from previous exec
+    (format t "~%~%~%~%~%")
+
+    (print-ln "apple")
+    (print-ln "banana")
+    (print-ln "pomegranate")
+
+)
+(main)
