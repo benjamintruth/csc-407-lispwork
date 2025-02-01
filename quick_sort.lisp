40702155ac95610e6572db357c5ea7a85ccf41e8
shaharnav1 <152940276+shaharnav1@users.noreply.github.com>
2025-02-01 08:51:22 -0500

    Update quick_sort.lisp

--- quick_sort.lisp
+++ quick_sort.lisp
@@ -1,10 +1,13 @@
 (defun quicksort (lst)
-  (if (or (null lst) (null (cdr lst)))
+  ;; Quick sort implementation in Lisp
+  (if (or (null lst) (null (cdr lst))) ; Base case: empty or single-element list is already sorted
       lst
-      (let* ((pivot (car lst))
-             (rest (cdr lst))
+      (let* ((pivot (car lst)) ; Select pivot as the first element
+             (rest (cdr lst)) ; Remaining elements
+             ;; Partition elements into smaller and larger lists based on pivot
              (smaller (remove-if-not (lambda (x) (<= x pivot)) rest))
              (larger (remove-if-not (lambda (x) (> x pivot)) rest)))
+        ;; Recursively sort smaller and larger sublists and combine them
         (append (quicksort smaller)
                 (list pivot)
                 (quicksort larger)))))
