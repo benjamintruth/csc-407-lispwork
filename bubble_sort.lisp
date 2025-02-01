ce6d080fe014db0c3f01b5c02fff985b1f1f9d1a
shaharnav1 <152940276+shaharnav1@users.noreply.github.com>
2025-02-01 08:50:21 -0500

    Updated bubble_sort.lisp with comments

--- bubble_sort.lisp
+++ bubble_sort.lisp
@@ -1,9 +1,14 @@
 (defun bubblesort (lst)
+  ;; Bubble sort implementation in Lisp
   (labels ((bubble (lst)
-             (if (or (null (cdr lst)) ; Single element or empty list
-                     (< (car lst) (cadr lst)))
+             ;; Swap adjacent elements if needed and continue bubbling
+             (if (or (null (cdr lst)) ; Single element or empty list, no swap needed
+                     (< (car lst) (cadr lst))) ; Elements already in order
                  lst
+                 ;; Swap elements and continue sorting
                  (cons (cadr lst) (bubble (cons (car lst) (cddr lst)))))))
-    (if (null (cdr lst)) ; List with 0 or 1 element is already sorted
+    ;; Base case: If the list has 0 or 1 element, it's already sorted
+    (if (null (cdr lst)) 
         lst
+        ;; Recur with the rest of the list and perform bubbling
         (bubble (cons (car lst) (bubblesort (cdr lst)))))))
